c486530cc7c75015fe1112d0b5b3a65596172096
praew-ramita <73657019+praew-ramita@users.noreply.github.com>
2020-11-27 12:34:58 +0700

    Update connect1.php

--- connect1.php
+++ connect1.php
@@ -1,6 +1,6 @@
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'ramita63070147.mysql.database.azure.com', 'ramita63070147@ramita63070147', 'Rt0877003141', 'labitff', 3306);
+mysqli_real_connect($conn, 'ramita63070147.mysql.database.azure.com', 'ramita63070147@ramita63070147', 'Rt0877003141', 'labitf', 3306);
 if(mysqli_connect_errno($conn)){
     echo "connect fail".mysqli_connect_error();
 }
